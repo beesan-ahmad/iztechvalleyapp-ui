61c394c20b2a4870693cfbce75ff1f994687dd20
beesan-ahmad <abumfarreh@gmail.com>
2024-05-13 23:44:27 +0300

    Set A Custom Theme

---
+++ src/shared/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00939f',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#000000',
+      contrastText: '#FFFFFF',
+    },
+  },
+});
